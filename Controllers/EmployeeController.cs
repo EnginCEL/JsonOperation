b7f6a5613ebeb409e938e5a3217f012d8751338a
example <80245505+EnginCEL@users.noreply.github.com>
2022-06-15 23:30:15 +0300

    Proje dosyası ekle.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxJqueryJsonOperation.EF;
+using AjaxJqueryJsonOperation.Models;
+
+namespace AjaxJqueryJsonOperation.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetEmployeeDetail()
+        {
+
+            DAL _dal = new DAL();
+            List<Employee> ListOfEmployee = new List<Employee>();
+            ListOfEmployee = _dal.Employees.ToList();
+            return Json(ListOfEmployee, JsonRequestBehavior.AllowGet);
+        }
+        public PartialViewResult Create()
+        {
+            return PartialView("Create");
+
+        }
+        [HttpPost]
+        public JsonResult Create(Employee _employee)
+        {
+            DAL _dal = new DAL();
+            _dal.Employees.Add(_employee);
+            _dal.SaveChanges();
+            return Json(_employee, JsonRequestBehavior.AllowGet);
+
+        }
+        [HttpGet]
+        public PartialViewResult Edit(int ID)
+        {
+            DAL _dal = new DAL();
+            Employee _employee = new Employee();
+            _employee = _dal.Employees.Find(ID);
+            return PartialView(_employee);
+
+
+        }
+        [HttpPost]
+        public JsonResult Edit(Employee _employee)
+        {
+            DAL _dal = new DAL();
+            _dal.Entry(_employee).State = EntityState.Modified;
+            _dal.SaveChanges();
+            return Json(_employee, JsonRequestBehavior.AllowGet);
+
+        }
+        [HttpGet]
+        public JsonResult Delete(int ID)
+        {
+
+            DAL _dal = new DAL();
+            Employee _employee = new Employee();
+            _employee = _dal.Employees.Find(ID);
+            _dal.Entry(_employee).State = EntityState.Deleted;
+            _dal.SaveChanges();
+            return Json(_employee, JsonRequestBehavior.AllowGet);
+        }
+     
+    }
+}
